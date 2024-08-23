41d33e6266f2ff28bbefc37a5c8e17f297a906db
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-23 04:53:01 +0200

    feat: hello world (https://youtu.be/2JjFCkD8Ua0?feature=shared)

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /**
+     * Commentaire
+     * multiligne
+     */
+    cout << "Hello, World!" << endl;
+
+    int a = 33;
+    int b = 44;
+    int c = 55;
+
+//    cout << a;
+//    cout << b;
+//    cout << c;
+//    cout << endl;
+
+    cout << a << " " << b << " " << c << endl;
+
+    return 0;
+}
